d9760486bb371f44241f1e35216e2bebdebbe7bb
AryehRotberg <aryeh.rotberg@gmail.com>
2023-07-17 10:17:38 +0300

    Modified app_utils.py

--- app.py
+++ app.py
@@ -14,62 +14,65 @@ course_ratings_df = pd.read_csv('data/raw/365_course_ratings.csv')
 student_learning_df.date_watched = pd.to_datetime(student_learning_df.date_watched)
 course_ratings_df.date_rated = pd.to_datetime(course_ratings_df.date_rated)
 
-student_country = app_utils().get_student_country()
-course_minutes_watched = app_utils().get_course_minutes_watched()
+# student_country = app_utils().get_student_country()
+# course_minutes_watched = app_utils().get_course_minutes_watched()
 
 st.title('365 Learning Data Challenge Dashboard')
 
 st.sidebar.title('Parameters')
 
 user_type_option = st.sidebar.selectbox('Select User Type', ('All', 'Free', 'Paid'))
-user_subscription_type_option = st.sidebar.selectbox('Select User Subscription Type', ('All', 'Monthly', 'Quarterly', 'Annual'))
+subscription_type_option = st.sidebar.selectbox('Select User Subscription Type', ('All', 'Monthly', 'Quarterly', 'Annual'))
 country_option = st.sidebar.selectbox('Select Country', ('All', 'Country 1', 'Country 2'))
 
-if user_subscription_type_option == 'All' and user_type_option == 'All' and country_option == 'All':
-    student_country = student_country[['student_country']].groupby('student_country').size().reset_index().sort_values(by=0, ascending=False).rename(columns={0: 'students'})
+# if subscription_type_option == 'All' and user_type_option == 'All' and country_option == 'All':
+#     student_country = student_country[['student_country']].groupby('student_country').size().reset_index().sort_values(by=0, ascending=False).rename(columns={0: 'students'})
 
-    country_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df[['student_id', 'minutes_watched']], on='student_id', how='left')
-    country_minutes_watched = country_minutes_watched[['student_country', 'minutes_watched']].groupby('student_country').sum().reset_index().sort_values(by='minutes_watched', ascending=False)
+#     country_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df[['student_id', 'minutes_watched']], on='student_id', how='left')
+#     country_minutes_watched = country_minutes_watched[['student_country', 'minutes_watched']].groupby('student_country').sum().reset_index().sort_values(by='minutes_watched', ascending=False)
 
-    platform = pd.merge(student_country, country_minutes_watched, on='student_country')
+#     platform = pd.merge(student_country, country_minutes_watched, on='student_country')
 
-    monthly_average_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df, on='student_id', how='left')
-    monthly_average_minutes_watched['month'] = monthly_average_minutes_watched.date_watched.apply(lambda date : date.month)
-    monthly_average_minutes_watched['average_minutes_watched'] = monthly_average_minutes_watched.minutes_watched
-    monthly_average_minutes_watched = monthly_average_minutes_watched[['month', 'minutes_watched', 'average_minutes_watched']].groupby('month').agg({'minutes_watched': 'sum', 'average_minutes_watched': 'mean'}).reset_index().round(2)
+#     monthly_average_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df, on='student_id', how='left')
+#     monthly_average_minutes_watched['month'] = monthly_average_minutes_watched.date_watched.apply(lambda date : date.month)
+#     monthly_average_minutes_watched['average_minutes_watched'] = monthly_average_minutes_watched.minutes_watched
+#     monthly_average_minutes_watched = monthly_average_minutes_watched[['month', 'minutes_watched', 'average_minutes_watched']].groupby('month').agg({'minutes_watched': 'sum', 'average_minutes_watched': 'mean'}).reset_index().round(2)
 
-else:
-    if user_type_option == 'Free':
-        paid = False
-    else:
-        paid = True
+# else:
+#     if user_type_option == 'Free':
+#         paid = False
+#     else:
+#         paid = True
     
-    student_country = student_country[(student_country.purchase_type == user_subscription_type_option) & (student_country.paid == paid)]
-    student_country = student_country[['student_country']].groupby('student_country').size().reset_index().sort_values(by=0, ascending=False).rename(columns={0: 'students'})
+#     student_country = student_country[(student_country.purchase_type == subscription_type_option) & (student_country.paid == paid)]
+#     student_country = student_country[['student_country']].groupby('student_country').size().reset_index().sort_values(by=0, ascending=False).rename(columns={0: 'students'})
 
-    country_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df[['student_id', 'minutes_watched']], on='student_id', how='left')
-    country_minutes_watched = country_minutes_watched[(country_minutes_watched.purchase_type == user_subscription_type_option) & (country_minutes_watched.paid == paid)]
-    country_minutes_watched = country_minutes_watched[['student_country', 'minutes_watched']].groupby('student_country').sum().reset_index().sort_values(by='minutes_watched', ascending=False)
+#     country_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df[['student_id', 'minutes_watched']], on='student_id', how='left')
+#     country_minutes_watched = country_minutes_watched[(country_minutes_watched.purchase_type == subscription_type_option) & (country_minutes_watched.paid == paid)]
+#     country_minutes_watched = country_minutes_watched[['student_country', 'minutes_watched']].groupby('student_country').sum().reset_index().sort_values(by='minutes_watched', ascending=False)
 
-    monthly_average_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df, on='student_id', how='left')
-    monthly_average_minutes_watched = monthly_average_minutes_watched[(monthly_average_minutes_watched.purchase_type == user_subscription_type_option) & (monthly_average_minutes_watched.paid == paid)]
-    monthly_average_minutes_watched['month'] = monthly_average_minutes_watched.date_watched.apply(lambda date : date.month)
-    monthly_average_minutes_watched['average_minutes_watched'] = monthly_average_minutes_watched.minutes_watched
-    monthly_average_minutes_watched = monthly_average_minutes_watched[['month', 'minutes_watched', 'average_minutes_watched']].groupby('month').agg({'minutes_watched': 'sum', 'average_minutes_watched': 'mean'}).reset_index().round(2)
+#     monthly_average_minutes_watched = pd.merge(merged_student_info_purchase, student_learning_df, on='student_id', how='left')
+#     monthly_average_minutes_watched = monthly_average_minutes_watched[(monthly_average_minutes_watched.purchase_type == subscription_type_option) & (monthly_average_minutes_watched.paid == paid)]
+#     monthly_average_minutes_watched['month'] = monthly_average_minutes_watched.date_watched.apply(lambda date : date.month)
+#     monthly_average_minutes_watched['average_minutes_watched'] = monthly_average_minutes_watched.minutes_watched
+#     monthly_average_minutes_watched = monthly_average_minutes_watched[['month', 'minutes_watched', 'average_minutes_watched']].groupby('month').agg({'minutes_watched': 'sum', 'average_minutes_watched': 'mean'}).reset_index().round(2)
 
-    platform = pd.merge(student_country, country_minutes_watched, on='student_country')
+#     platform = pd.merge(student_country, country_minutes_watched, on='student_country')
 
 
+course_minutes_watched = app_utils(user_type_option, subscription_type_option, country_option).get_course_minutes_watched()
 st.write(course_minutes_watched.head())
 
-# st.write('Top 5 Largest Number of Users')
+student_country = app_utils(user_type_option, subscription_type_option, country_option).get_student_country()
+
+st.write('Top 5 Largest Number of Users')
 st.write(px.bar(student_country.head(), x='students', y='student_country', orientation='h', color='student_country', title='Top 5 Largest Number of Users'))
 
-st.write('')
-st.write(px.bar(platform.head(), x='minutes_watched', y='student_country', orientation='h', color='student_country'))
+# st.write('')
+# st.write(px.bar(platform.head(), x='minutes_watched', y='student_country', orientation='h', color='student_country'))
 
-st.write('')
-fig = px.line(monthly_average_minutes_watched, x='month', y='average_minutes_watched')
-fig.add_bar(x=monthly_average_minutes_watched.month, y=monthly_average_minutes_watched.minutes_watched)
-# st.write(px.bar(monthly_average_minutes_watched, x='month', y='minutes_watched'))
-st.write(fig)
+# st.write('')
+# fig = px.line(monthly_average_minutes_watched, x='month', y='average_minutes_watched')
+# fig.add_bar(x=monthly_average_minutes_watched.month, y=monthly_average_minutes_watched.minutes_watched)
+# # st.write(px.bar(monthly_average_minutes_watched, x='month', y='minutes_watched'))
+# st.write(fig)
--- app_utils.py
+++ app_utils.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 class app_utils:
-    def __init__(self):    
+    def __init__(self, user_type_option: str, subscription_type_option: str, country_option: str):
+        self.user_type_option = user_type_option
+        self.subscription_type_option = subscription_type_option
+        self.country_option = country_option
+
         self.merged_student_info_purchase = pd.read_csv('notebook/data/processed/merged_student_info_purchase.csv')
         self.student_learning_df = pd.read_csv('data/raw/365_student_learning.csv')
         self.course_info_df = pd.read_csv('data/raw/365_course_info.csv')
@@ -11,8 +15,31 @@ class app_utils:
         self.student_learning_df.date_watched = pd.to_datetime(self.student_learning_df.date_watched)
         self.course_ratings_df.date_rated = pd.to_datetime(self.course_ratings_df.date_rated)
     
+    def modify_options(self, df):
+        if self.user_type_option == 'Free':
+            user_type_option = 'Free'
+            subscription_type_option = df['purchase_type']
+        
+        else:
+            user_type_option = self.user_type_option
+            subscription_type_option = self.subscription_type_option
+            country_option = self.country_option
+        
+        if self.user_type_option == 'All':
+            user_type_option = df['paid']
+        if self.subscription_type_option == 'All':
+            subscription_type_option = df['purchase_type']
+        if self.country_option == 'All':
+            country_option = df['student_country']
+        
+        return user_type_option, subscription_type_option, country_option
+            
     def get_student_country(self):
-        return self.merged_student_info_purchase
+        user_type_option, subscription_type_option, country_option = self.modify_options(self.merged_student_info_purchase)
+        student_country = self.merged_student_info_purchase[(self.merged_student_info_purchase.paid == user_type_option)
+                                                 & (self.merged_student_info_purchase.purchase_type == subscription_type_option)
+                                                 & (self.merged_student_info_purchase.student_country == country_option)]
+        return student_country[['student_country']].groupby('student_country').size().reset_index().sort_values(by=0, ascending=False).rename(columns={0: 'students'})
     
     def get_course_minutes_watched(self):
         course_info_ratings = pd.merge(self.course_info_df, self.course_ratings_df[['course_id', 'course_rating']], on='course_id')
@@ -27,3 +54,5 @@ class app_utils:
         course_minutes_watched = course_minutes_watched.drop('course_id', axis=1)
 
         return course_minutes_watched
+    
+
