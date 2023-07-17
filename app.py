cf33c28cae139395647d1b7dfe37619c9e2da08b
AryehRotberg <aryeh.rotberg@gmail.com>
2023-07-17 23:42:09 +0300

    Updated dashboard app

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 import plotly.express as px
+import plotly.graph_objects as go
+
 import streamlit as st
 
 from app_utils import app_utils
 
 
+st.set_page_config(layout='wide')
 st.title('365 Learning Data Challenge Dashboard')
 
 st.sidebar.title('Parameters')
@@ -13,24 +16,35 @@ st.sidebar.title('Parameters')
 user_type_option = st.sidebar.selectbox('Select User Type', ('All', 'Free', 'Paid'))
 subscription_type_option = st.sidebar.selectbox('Select User Subscription Type', ('All', 'Monthly', 'Quarterly', 'Annual'))
 
-countries_list = pd.read_csv('notebook/data/raw/365_student_info.csv').student_country.unique().tolist()
 countries_list = ['All']
 countries_list.extend(pd.read_csv('notebook/data/raw/365_student_info.csv').student_country.unique().tolist())
 
 country_option = st.sidebar.selectbox('Select Country', countries_list)
 
-course_minutes_watched = app_utils(user_type_option, subscription_type_option, country_option).get_course_minutes_watched()
-student_country = app_utils(user_type_option, subscription_type_option, country_option).get_student_country()
-platform = app_utils(user_type_option, subscription_type_option, country_option).get_platform_minutes_watched()
-monthly_average_minutes_watched = app_utils(user_type_option, subscription_type_option, country_option).get_monthly_average_minutes_watched()
+utils = app_utils(user_type_option, subscription_type_option, country_option)
+
+course_minutes_watched = utils.get_course_minutes_watched()
+student_country = utils.get_student_country()
+platform = utils.get_platform_minutes_watched()
+monthly_average_minutes_watched = utils.get_monthly_average_minutes_watched()
+students_registered_onboarded = utils.get_students_registered_onboarded()
 
 st.write(course_minutes_watched.head())
-st.write(px.bar(student_country.head(), x='students', y='student_country', orientation='h', color='student_country', title='Chart 1.1 - Top 5 Largest Number of Users'))
-st.write(px.bar(platform.head(), x='minutes_watched', y='student_country', orientation='h', color='student_country', title='Chart 1.2 - Minutes watched on the platform by users'))
-st.write(px.bar(monthly_average_minutes_watched, x='month', y='average_minutes_watched'))
 
-# st.write('')
-# fig = px.line(monthly_average_minutes_watched, x='month', y='average_minutes_watched')
-# fig.add_bar(x=monthly_average_minutes_watched.month, y=monthly_average_minutes_watched.minutes_watched)
-# # st.write(px.bar(monthly_average_minutes_watched, x='month', y='minutes_watched'))
-# st.write(fig)
+col1_chart, col2_chart = st.columns(2, gap='large')
+col1_chart.write(px.bar(student_country.head(), x='students', y='student_country', orientation='h', color='student_country', title='Chart 1.1 - Top 5 Largest Number of Users'))
+col2_chart.write(px.bar(platform.head(), x='minutes_watched', y='student_country', orientation='h', color='student_country', title='Chart 1.2 - Minutes watched on the platform by users'))
+
+col3_chart, col4_chart = st.columns(2, gap='large')
+col3_chart.write(go.Figure(data=[go.Bar(name='Minutes Watched', x=monthly_average_minutes_watched.month, y=monthly_average_minutes_watched.minutes_watched, offsetgroup=1),
+                                 go.Line(name='Average Minutes Watched', x=monthly_average_minutes_watched.month, y=monthly_average_minutes_watched.average_minutes_watched, offsetgroup=2)])
+                                 .update_layout(title='Chart 1.3 - Minutes Watched by Month'))
+col4_chart.write(go.Figure(data=[go.Bar(name='Students', x=students_registered_onboarded.month, y=students_registered_onboarded.students, offsetgroup=1),
+                                 go.Bar(name='Onboarded', x=students_registered_onboarded.month, y=students_registered_onboarded.onboarded, offsetgroup=2)])
+                                 .update_layout(title='Chart 1.4 - Number of Registered Students Compared to Onboarded'))
+
+col1_kpi, col2_kpi, col3_kpi, col4_kpi = st.columns(4)
+col1_kpi.metric('Registered Students', value=utils.get_registered_students_kpi())
+col2_kpi.metric('Minutes Watched', value=utils.get_minutes_watched_kpi())
+col3_kpi.metric('Average Minutes Watched', value=utils.get_average_minutes_watched_kpi())
+col4_kpi.metric('Onboarded from Registered', value=f'{utils.get_onboarded_from_registered_kpi()}%')
--- app_utils.py
+++ app_utils.py
@@ -7,19 +7,24 @@ class app_utils:
         self.country_option = country_option
 
         self.student_info_df = pd.read_csv('notebook/data/raw/365_student_info.csv')
-        self.merged_student_info_purchase = pd.read_csv('notebook/data/processed/merged_student_info_purchase.csv')
         self.student_learning_df = pd.read_csv('notebook/data/raw/365_student_learning.csv')
         self.course_info_df = pd.read_csv('notebook/data/raw/365_course_info.csv')
         self.course_ratings_df = pd.read_csv('notebook/data/raw/365_course_ratings.csv')
+        self.student_engagement_df = pd.read_csv('notebook/data/raw/365_student_engagement.csv')
+        self.merged_student_info_purchase = pd.read_csv('notebook/data/processed/merged_student_info_purchase.csv')
 
         self.student_learning_df.date_watched = pd.to_datetime(self.student_learning_df.date_watched)
         self.course_ratings_df.date_rated = pd.to_datetime(self.course_ratings_df.date_rated)
+        self.student_engagement_df.date_engaged = pd.to_datetime(self.student_engagement_df.date_engaged)
         self.merged_student_info_purchase.date_registered = pd.to_datetime(self.merged_student_info_purchase.date_registered)
+
     
     def get_country_list(self):
         return self.student_info_df.student_country.unique().tolist()
     
     def modify_options(self, df):
+        country_option = self.country_option
+
         if self.user_type_option == 'Free':
             user_type_option = False
             subscription_type_option = df['purchase_type']
@@ -27,7 +32,6 @@ class app_utils:
         else:
             user_type_option = self.user_type_option
             subscription_type_option = self.subscription_type_option
-            country_option = self.country_option
         
         if self.user_type_option == 'Paid':
             user_type_option = True
@@ -89,3 +93,45 @@ class app_utils:
         monthly_average_minutes_watched = monthly_average_minutes_watched[['month', 'minutes_watched', 'average_minutes_watched']].groupby('month').agg({'minutes_watched': 'sum', 'average_minutes_watched': 'mean'}).reset_index().round(2)
 
         return monthly_average_minutes_watched
+    
+    def get_students_registered_onboarded(self):
+        monthly_registered_students = pd.merge(self.merged_student_info_purchase, self.student_engagement_df, on='student_id', how='left')
+        monthly_registered_students.date_registered = pd.to_datetime(monthly_registered_students.date_registered)
+
+        monthly_onboarded_students = monthly_registered_students.copy()
+
+        monthly_registered_students = monthly_registered_students.drop_duplicates(subset='student_id')
+
+        user_type_option, subscription_type_option, country_option = self.modify_options(monthly_registered_students)
+        monthly_registered_students = monthly_registered_students[(monthly_registered_students.paid == user_type_option)
+                                                                  & (monthly_registered_students.purchase_type == subscription_type_option)
+                                                                  & (monthly_registered_students.student_country == country_option)]
+
+        monthly_registered_students['month'] = monthly_registered_students.date_registered.apply(lambda date : date.month)
+        monthly_registered_students = monthly_registered_students[['month']].groupby(['month']).size().reset_index()
+        monthly_registered_students = monthly_registered_students.rename(columns={0: 'students'})
+
+        user_type_option, subscription_type_option, country_option = self.modify_options(monthly_onboarded_students)
+        monthly_onboarded_students = monthly_onboarded_students[(monthly_onboarded_students.paid == user_type_option)
+                                                                  & (monthly_onboarded_students.purchase_type == subscription_type_option)
+                                                                  & (monthly_onboarded_students.student_country == country_option)]
+
+        monthly_onboarded_students['month'] = monthly_onboarded_students.date_engaged.apply(lambda date : date.month)
+        monthly_onboarded_students = monthly_onboarded_students[['month']].groupby(['month']).size().reset_index()
+        monthly_onboarded_students = monthly_onboarded_students.rename(columns={0: 'onboarded'})
+
+        students_registered_onboarded = pd.merge(monthly_registered_students, monthly_onboarded_students, on='month')
+
+        return students_registered_onboarded
+    
+    def get_registered_students_kpi(self):
+        return self.student_info_df.student_id.nunique()
+    
+    def get_minutes_watched_kpi(self):
+        return self.student_learning_df.minutes_watched.sum().round()
+    
+    def get_average_minutes_watched_kpi(self):
+        return (self.get_minutes_watched_kpi() / self.student_learning_df.student_id.nunique()).round()
+    
+    def get_onboarded_from_registered_kpi(self):
+        return round(self.student_engagement_df.student_id.nunique() / self.student_info_df.student_id.nunique(), 3) * 100
