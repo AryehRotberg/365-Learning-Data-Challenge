7f9f25b1785816ac1dc37e262932315513f450fc
AryehRotberg <aryeh.rotberg@gmail.com>
2023-07-28 17:38:52 +0300

    Modified machine learning pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 
+import pickle
+import json
+
+import mlflow
+
 from xgboost import XGBClassifier
 
 from sklearn.linear_model import LogisticRegression
@@ -9,6 +14,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, f1_score
 
 from dataclasses import dataclass
 
@@ -25,22 +32,115 @@ class ModelTrainer:
     def __init__(self):
         self.config = ModelTrainerConfig()
     
-    # def get_gridsearch_dict():
-    #     grid_search = {
-    #         'rfc': {'classifier': RandomForestClassifier(),
-    #         'params': {
-    #             'bootstrap': [True, False],
-    #             'max_depth': [10, 20, 30, 40, 50, None],
-    #             'max_features': ['auto', 'sqrt'],
-    #             'min_samples_leaf': [1, 2, 4],
-    #             'min_samples_split': [2, 5, 10],
-    #             'n_estimators': [200, 400, 600]
-    #         }}
-    #     }
+    def get_gridsearch_dict(self):
+        # self.grid_search = {
+        #     'logistic_regression': {
+        #         'classifier': LogisticRegression(), 'params': {
+        #             'C': np.logspace(-4, 4, 20),
+        #             'penalty': ['l1', 'l2'],
+        #             'solver': ['newton-cg', 'lbfgs', 'liblinear']
+        #         }
+        #     },
 
-    #     grid_search = {
-    #         'logistic_regression': {'classifier': LogisticRegression(), 'params': {'C': np.logspace(-4, 4, 20)}},
-    #         'rfc': {'classifier': RandomForestClassifier(), 'params': {'n_estimators': [1, 5, 10], 'max_depth': [1, 5, 10]}},
-    #         'xgb_classifier': {'classifier': XGBClassifier(), 'params': {'n_estimators': [1, 5, 10], 'max_depth': [1, 5, 10]}},
-    #         'knn': {'classifier': KNeighborsClassifier(), 'params': {'n_neighbors': list(range(1, 10))}}
-    #     }
+        #     'rfc': {
+        #         'classifier': RandomForestClassifier(), 'params': {
+        #             'bootstrap': [True, False],
+        #             'max_depth': [1, 5, 10, 20, 30, None],
+        #             'max_features': ['log2', 'sqrt'],
+        #             'min_samples_split': [2, 5, 10],
+        #             'n_estimators': [1, 5, 10, 20, 30]
+        #         }
+        #     },
+
+        #     'xgb_classifier': {
+        #         'classifier': XGBClassifier(), 'params': {
+        #             'min_child_weight': [1, 5, 10],
+        #             'gamma': [0.5, 1, 1.5, 2, 5],
+        #             'subsample': [0.6, 0.8, 1.0],
+        #             'colsample_bytree': [0.6, 0.8, 1.0],
+        #             'max_depth': [3, 4, 5]
+        #         }
+        #     },
+
+        #     'knn': {
+        #         'classifier': KNeighborsClassifier(), 'params': {
+        #             'n_neighbors': [5, 7, 9, 11, 13, 15],
+        #             'weights' : ['uniform','distance'],
+        #             'metric' : ['minkowski','euclidean','manhattan']
+        #         }
+        #     }
+        # }
+
+        self.grid_search = {
+            'rfc': {
+                'classifier': RandomForestClassifier(), 'params': {
+                    'bootstrap': [True, False],
+                    'max_depth': [1, 5, 10, 20, 30, None],
+                    'max_features': ['log2', 'sqrt'],
+                    'min_samples_split': [2, 5, 10],
+                    'n_estimators': [1, 5, 10, 20, 30]
+                }
+            }
+        }
+
+        return self.grid_search
+    
+    def get_tuned_models_scores(self, grid_search: dict, verbose=False):
+        scores = []
+
+        for classifier_name, classifier_params in grid_search.items():
+            gs = GridSearchCV(classifier_params['classifier'],
+                            classifier_params['params'],
+                            cv=5,
+                            scoring=['f1_micro', 'roc_auc'],
+                            refit='f1_micro',
+                            verbose=10 if verbose else 0)
+            
+            gs.fit(self.config.X_train, self.config.y_train.values.ravel())
+            
+            scores.append({
+                'classifier': classifier_name,
+                'best_score': gs.best_score_,
+                'best_params': gs.best_params_,
+                'best_estimator': gs.best_estimator_
+            })
+        
+        scores_df = pd.DataFrame(scores).sort_values(by='best_score', ascending=False)
+        return scores_df
+    
+    def get_best_classifier(self, scores_df: pd.DataFrame):
+        classifier = scores_df.loc[scores_df.best_score.idxmax()].best_estimator
+        params = scores_df.loc[scores_df.best_score.idxmax()].best_params
+
+        classifier.fit(self.config.X_train, self.config.y_train.values.ravel())
+
+        return classifier, params
+    
+    def save_best_classifier(self, classifier, params):
+        y_pred = classifier.predict(self.config.X_test)
+
+        report = pd.DataFrame(classification_report(self.config.y_test, y_pred, output_dict=True)).transpose()
+        report.to_csv('data/processed/RandomForestClassifier_report.csv', index=False)
+
+        mlflow.set_experiment('365 Learning Data Challenge - Machine Learning')
+
+        with mlflow.start_run():
+            for param_type, value in params.items():
+                mlflow.log_param(param_type, value)
+            
+            mlflow.log_artifact('data/processed/RandomForestClassifier_report.csv')
+
+            mlflow.log_metric('f1_score', f1_score(self.config.y_test, y_pred))
+            mlflow.log_metric('roc_auc_score', roc_auc_score(self.config.y_test, y_pred))
+
+            mlflow.sklearn.log_model(classifier, 'RandomForestClassifier', registered_model_name='RandomForestClassifier')
+
+
+if __name__ == '__main__':
+    model_trainer = ModelTrainer()
+
+    grid_search = model_trainer.get_gridsearch_dict()
+    scores = model_trainer.get_tuned_models_scores(grid_search, verbose=True)
+    classifier, params = model_trainer.get_best_classifier(scores)
+
+    model_trainer.save_best_classifier(classifier, params)
