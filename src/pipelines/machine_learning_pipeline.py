bb16ee42b0282e8fddb888079e34041c233b6787
AryehRotberg <aryeh.rotberg@gmail.com>
2023-08-06 15:59:12 +0300

    Modified machine learning pipeline

--- src/pipelines/machine_learning_pipeline.py
+++ src/pipelines/machine_learning_pipeline.py
@@ -4,6 +4,8 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
+import mlflow
+
 logging.basicConfig(
     filename='logs.log',
     format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s', 
@@ -13,26 +15,27 @@ logging.basicConfig(
 # Data Ingestion
 logging.info('Preparing data for machine learning.')
 
-di = DataIngestion()
-di.prepare_dataframe()
-di.to_csv('data/processed/ml_dataset.csv')
+ingestion = DataIngestion()
+ingestion.prepare_dataframe()
+ingestion.to_csv('data/processed/ml_dataset.csv')
 
 # Data Transformation
 logging.info('Transforming data.')
 
-dt = DataTransformation()
-dt.transform_data(scaler='standard',
-                  resampler='ros|rus')
+transformation = DataTransformation()
+transformation.transform_data(scaler='standard',
+                              resampler='ros|rus')
 
-dt.to_csv(train_directory='data/train',
-          test_directory='data/test')
+transformation.to_csv(train_directory='data/train',
+                      test_directory='data/test')
 
 # Model Training
 logging.info('Model hyperparameter tuning.')
 
-mt = ModelTrainer()
+model_training = ModelTrainer()
 
-grid_search = mt.get_gridsearch_dict()
-scores = mt.get_tuned_models_scores(grid_search, verbose=True)
-classifier, params = mt.get_best_classifier(scores)
-mt.save_best_classifier(classifier, params)
+grid_search = model_training.get_gridsearch_dict()
+scores = model_training.get_tuned_models_scores(grid_search, verbose=True)
+classifier, params = model_training.get_best_classifier(scores)
+
+model_training.save_best_classifier(classifier, params)
