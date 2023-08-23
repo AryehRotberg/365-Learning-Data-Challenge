782b39b94ba3ba6f7517875523f0e7ab01bd03a9
AryehRotberg <aryeh.rotberg@gmail.com>
2023-08-24 00:21:12 +0300

    Modified optuna test pipeline

--- src/pipelines/optuna_test_pipeline.py
+++ src/pipelines/optuna_test_pipeline.py
@@ -15,20 +15,7 @@ X_test = pd.read_csv('data/test/X_test.csv').values
 y_train = pd.read_csv('data/train/y_train.csv').values
 y_test = pd.read_csv('data/test/y_test.csv').values
 
-def objective(trial: optuna.trial.Trial):
-    n_neighbors = trial.suggest_categorical('n_neighbors', [5, 7, 9, 11, 13, 15])
-    weights = trial.suggest_categorical('weights', ['uniform', 'distance'])
-    metric = trial.suggest_categorical('metric', ['minkowski', 'euclidean', 'manhattan'])
-
-    classifier = KNeighborsClassifier(n_neighbors=n_neighbors,
-                                      weights=weights,
-                                      metric=metric)
-    
-    score = cross_val_score(classifier, X_train, y_train.ravel(), n_jobs=-1, cv=5, scoring='f1')
-    
-    return score.mean()
-
-def objective(trial: optuna.trial.Trial):
+def objective_rfc(trial: optuna.trial.Trial):
     bootstrap = trial.suggest_categorical('bootstrap', [True, False])
     max_depth = trial.suggest_categorical('max_depth', [1, 5, 10, 20, 30, None])
     max_features = trial.suggest_categorical('max_features', ['log2', 'sqrt'])
@@ -42,11 +29,22 @@ def objective(trial: optuna.trial.Trial):
                                         n_estimators=n_estimators)
     
     score = cross_val_score(classifier, X_train, y_train.ravel(), n_jobs=-1, cv=5, scoring='f1')
+    return score.mean()
 
+def objective_knn(trial: optuna.trial.Trial):
+    n_neighbors = trial.suggest_categorical('n_neighbors', [5, 7, 9, 11, 13, 15])
+    weights = trial.suggest_categorical('weights', ['uniform', 'distance'])
+    metric = trial.suggest_categorical('metric', ['minkowski', 'euclidean', 'manhattan'])
+
+    classifier = KNeighborsClassifier(n_neighbors=n_neighbors,
+                                      weights=weights,
+                                      metric=metric)
+    
+    score = cross_val_score(classifier, X_train, y_train.ravel(), n_jobs=-1, cv=5, scoring='f1')
     return score.mean()
 
 study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=100)
+study.optimize(objective_knn, n_trials=100)
 
 print(study.best_params)
 print(study.best_value)
